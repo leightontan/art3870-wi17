bef12f1b841e43a142075e0bac0e4c6868811690
leightontan <ltan23@horizon.csueastbay.edu>
2017-03-16 22:33:46 -0700

    Add files via upload

--- longscroll/js/longscroll.js
+++ longscroll/js/longscroll.js
@@ -7,44 +7,53 @@ $(document).ready(function() {
     $(window).scroll(function(){//start of scroll
 
 //trigger animation at certain point
+//person going up
 if($(window).scrollTop() >= $(document).height() - $(window).height() - 30){
       $(".person").css("-webkit-animation-play-state", "running")
 
     }
+//light appearing
     if($(window).scrollTop() >= $(document).height() - $(window).height() - 50){
           $(".light").css("-webkit-animation-play-state", "running")
-
+//Text box appearing
         }
-        if($(window).scrollTop() >= 1010){
-              $(".box").css("-webkit-animation-play-state", "running")
+    if($(window).scrollTop() >= 1010){
+          $(".box").css("-webkit-animation-play-state", "running")
 
             }
-            if($(window).scrollTop() >= 2810){
-                  $(".box1").css("-webkit-animation-play-state", "running")
+    if($(window).scrollTop() >= 2810){
+          $(".box1").css("-webkit-animation-play-state", "running")
 
                 }
-                if($(window).scrollTop() >= 5510){
-                      $(".box2").css("-webkit-animation-play-state", "running")
+    if($(window).scrollTop() >= 5510){
+    $(".box2").css("-webkit-animation-play-state", "running")
 
-                    }
-                    if($(window).scrollTop() >= 6850){
-                          $(".box3").css("-webkit-animation-play-state", "running")
+  }
+
+    if($(window).scrollTop() >= 6850){
+    $(".box3").css("-webkit-animation-play-state", "running")
 
                         }
-                        if($(window).scrollTop() >= 8800){
-                              $(".box4").css("-webkit-animation-play-state", "running")
+    if($(window).scrollTop() >= 8800){
+    $(".box4").css("-webkit-animation-play-state", "running")
 
-                            }
-      //update variable with the number of pixels scrolled
-      var fromTop = $(window).scrollTop();
 
-      // ||||||||||| ASTEROIDS SECTION |||||||||||||||||||||||||
-      $("#asteroid").css({
-                    "left": (-800 + fromTop)* 2.5 + "px"
-                  });
+    }
+    if($(window).scrollTop() >= 8550){
+    $(".box5").css("-webkit-animation-play-state", "running")
+
+                        }
+    //end of text box
+    //update variable with the number of pixels scrolled
+    var fromTop = $(window).scrollTop();
+
+    // ||||||||||| ASTEROIDS SECTION |||||||||||||||||||||||||
+    $("#asteroid").css({
+    "left": (-800 + fromTop)* 2.5 + "px"
+    });
 
       // ||||||||||| SPACESHIP SECTION |||||||||||||||||||||||||
-
+      //Spaceship disappearing
       if(fromTop >= 3150){
         $("#spaceship").css({
                       "display":"none"
@@ -57,7 +66,7 @@ if($(window).scrollTop() >= $(document).height() - $(window).height() - 30){
         }
 
 
-
+      //Spaceship reppearing
       if(fromTop >= 4800){
         $("#spaceship").text("You scrolled " + fromTop +"px");
         $("#spaceship").css({
@@ -87,7 +96,7 @@ if(fromTop >= 4850){
       $(".cloud").css({
         "top" : (5250 + fromTop) , "display":"none"
       });
-  }
+  }//End of cloud
   //Cloud1
   if(fromTop < 5250){
       $(".cloud1").css({
@@ -104,7 +113,7 @@ if(fromTop >= 4850){
         $(".cloud1").css({
           "top" : (5250 + fromTop) , "display":"none"
         });
-    }
+    }//End of cloud1
 //Cloud2
     if(fromTop < 5250){
         $(".cloud2").css({
@@ -121,7 +130,7 @@ if(fromTop >= 4850){
           $(".cloud2").css({
             "top" : (5250 + fromTop) , "display":"none"
           });
-      }
+      }//End of cloud2
 //Cloud3
       if(fromTop < 5250){
           $(".cloud3").css({
@@ -138,9 +147,9 @@ if(fromTop >= 4850){
             $(".cloud3").css({
               "top" : (5250 + fromTop) , "display":"none"
             });
-        }
+        }//End of cloud3
 
-
+//Bird flying
     $(".bird").css({
                   "left": (-5100 + fromTop) * 0.8 + "px"
                 });
